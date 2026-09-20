f237abbb46567376491d5b0797ae47cbaf846b01
Tanbhir Tahmid <tanbhir.tahmid@gmail.com>
2026-09-20 08:28:58 +0600

    fix one data type

--- Student Management System/src/Course.java
+++ Student Management System/src/Course.java
@@ -1,11 +1,11 @@
 public class Course {
     private String code;
     private String title;
-    private String creditHour;
+    private double creditHour;
     private String type;
     private boolean prerequisite;
 
-    public Course(String code, String title, String creditHour, String type, boolean prerequisite){
+    public Course(String code, String title, double creditHour, String type, boolean prerequisite){
 
     }
 }
